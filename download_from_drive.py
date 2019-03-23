124bc58061c8df9029f84470d261e0fc0306adf7
raffy-bekhit <raffybekhit@gmail.com>
2019-03-23 18:38:54 +0200

    modify prepreprocess

--- download_from_drive.py
+++ download_from_drive.py
@@ -35,7 +35,8 @@ def save_response_content(response, destination):
 
 if __name__ == "__main__":
 
-    ids_file = open("drive_zip_ids.txt",'r')
+    f = open("drive_zip_ids.txt",'r')
+    ids_file = f.readlines()
     for line in ids_file:
         id, name = line.split(",")
         download_file_from_google_drive(id,"./audio/"+name+".zip" )
