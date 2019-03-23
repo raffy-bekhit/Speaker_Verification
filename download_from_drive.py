ae33d58625cc87ea737f6dd9ef762611a43f5667
raffy-bekhit <raffybekhit@gmail.com>
2019-03-23 19:43:20 +0200

    modify prepreprocess

--- download_from_drive.py
+++ download_from_drive.py
@@ -2,7 +2,7 @@ import requests
 import sys
 from data_preprocess import save_spectrogram_tisv
 from zipfile import ZipFile
-
+import os
 
 def download_file_from_google_drive(id, destination):
     URL = "https://docs.google.com/uc?export=download"
