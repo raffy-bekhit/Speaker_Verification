78c016b816cbcc67ec2401252b0883ba7b2d1363
raffy-bekhit <raffybekhit@gmail.com>
2019-03-28 17:51:44 +0200

    add minor change

--- utils.py
+++ utils.py
@@ -65,7 +65,7 @@ def random_batch(speaker_num=config.N, utter_num=config.M, shuffle=True, noise_f
     else:
         np_file_list = os.listdir(path)
         total_speaker = len(np_file_list)
-		print(total_speaker)
+        print(total_speaker)
 
         if shuffle:
             selected_files = random.sample(np_file_list, speaker_num)  # select random N speakers
