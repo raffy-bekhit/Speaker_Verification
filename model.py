f76058b8cdd14c7815a539bfcf3937a28f743522
raffy-bekhit <raffybekhit@gmail.com>
2019-04-24 17:41:06 +0200

    update

--- model.py
+++ model.py
@@ -253,7 +253,7 @@ def output(model_path):
             S = sess.run(similarity_matrix, feed_dict={enroll:random_batch(shuffle=False, noise_filenum=1),
                                                        verif:random_batch(shuffle=False, noise_filenum=2)})
         else:
-            e = sess.run(enroll_embed, feed_dict={enroll:random_batch(shuffle=False)})
+            e = sess.run(enroll_embed, feed_dict={enroll:test_input()})
 
         print("embedding shape: " , e.shape)
         print("embedding: " , e)
