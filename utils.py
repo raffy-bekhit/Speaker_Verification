bd84a3143f702b721237627406abfedff4e13902
raffy-bekhit <raffybekhit@gmail.com>
2019-05-15 22:08:36 +0200

    update

--- utils.py
+++ utils.py
@@ -29,17 +29,17 @@ def test_input():
     utter_batch = []
     counter = 0
     corrupted = []
-    #cor_file = open("corrupted_filenames.txt","w+")
+    cor_file = open("corrupted_filenames.txt","w+")
     for file in selected_files:
         utters = np.load(os.path.join(path, file))        # load utterance spectrogram of selected speaker
         if utters.shape[0] != 0:
-            #print(utters.shape)
+            print(utters.shape)
             utter_batch.append(utters[0:config.M])
 
-#        else:
-            #corrupted.append(file)
-            #counter=counter+1
-            #shutil.move(os.path.join(path, file),"../corrupted_spectrograms/"+file)
+        else:
+            corrupted.append(file)
+            counter=counter+1
+            shutil.move(os.path.join(path, file),"../corrupted_spectrograms/"+file)
 
 
 
@@ -47,9 +47,9 @@ def test_input():
     utter_batch = np.concatenate(utter_batch, axis=0)     # utterance batch [batch(NM), n_mels, frames]
     print("shape: ",utter_batch.shape)
     #print("corrupted: ", counter)
-    #for cor_name in corrupted:
-    #    cor_file.write(cor_name+"\n")
-    #cor_file.close()
+    for cor_name in corrupted:
+        cor_file.write(cor_name+"\n")
+    cor_file.close()
     utter_batch = utter_batch[:,:,:160]               # for train session, fixed length slicing of input batch
 
     utter_batch = np.transpose(utter_batch, axes=(2,0,1))     # transpose [frames, batch, n_mels]
