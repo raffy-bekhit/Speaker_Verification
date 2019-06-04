d375a2a7309be970cad6151d70399c8d8dcbbef9
raffy-bekhit <raffybekhit@gmail.com>
2019-06-04 02:06:07 +0200

    add minor fix

--- model.py
+++ model.py
@@ -62,7 +62,7 @@ def train(path):
 
 #                if (checkpoint_state and checkpoint_state.model_checkpoint_path):
 #                    print('Loading checkpoint {}'.format(checkpoint_state.model_checkpoint_path))
-            saver = tf.train.import_meta_graph(os.path.join(path,"Check_Point/model.cpkt.meta"))
+            #saver = tf.train.import_meta_graph(os.path.join(path,"Check_Point/model.cpkt.meta"))
 
             ckpt = tf.train.load_checkpoint(os.path.join(path,"Check_Point/model.ckpt"))
             saver.restore(sess, ckpt)
