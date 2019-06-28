60187cdb8406ef91597ccf4acc36e912f9080434
Raffy Bekhit Moulazem <es-raffy.bekhit@alexu.edu.eg>
2019-06-28 15:22:25 +0200

    Update model.py

--- model.py
+++ model.py
@@ -208,7 +208,7 @@ def test(path):
 
         #ckpt = tf.train.get_checkpoint_state(path)
         #checkpoints =  ckpt.all_model_checkpoint_paths
-        i=49999
+        i=92499
         least_loss = 99999
         #print("checkpoints : ",checkpoints)
 
